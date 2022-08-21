64a431367302b4ca0131717a468ce7eaf2fffb44
ImVikasJi <vbhadoriya3@gmail.com>
2022-08-22 00:54:41 +0530

    Applying CRUD operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from './interface/user';
 import { UserService } from './service/user.service';
 
 @Component({
@@ -10,25 +11,51 @@ import { UserService } from './service/user.service';
 export class AppComponent implements OnInit {
   title = 'Angular-HTTP-API';
 
+  private user: User = {
+    id: 5,
+    name: 'Vikas Singh Bhadoriya',
+    username: 'Badri',
+    email: 'Sincere@gmail.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  };
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.onGetUsers();
-    this.onGetUser();
+    // this.onGetUser();
+    this.onUpdateUser();
+    this.ondeleteUser();
+    
   }
 
   // To get all the users form Json PlaceHolder APi
   onGetUsers() {
     this.userService.getUsers().subscribe({
       next: (data) => {
-        console.log(data);
+        console.table(data);
       },
       error: (data) => {
         console.log(data);
       },
-      complete: () => {console.log('Done Getting Users');}
-        
-      
+      complete: () => {
+        console.log('Done Getting Users');
+      },
     });
   }
 
@@ -41,7 +68,51 @@ export class AppComponent implements OnInit {
       error: (data) => {
         console.log(data);
       },
-      complete: () => {console.log('Done Getting User');}
+      complete: () => {
+        console.log('Done Getting User');
+      },
+    });
+  }
+
+  onCreateUser(): void {
+    this.userService.createUser(this.user).subscribe({
+      next: (user) => {
+        console.log(user);
+      },
+      error: (data) => {
+        console.log(data);
+      },
+      complete: () => {
+        console.log('Done Creating User');
+      },
+    });
+  }
+
+  onUpdateUser() {
+    this.userService.updateUser(this.user).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (data) => {
+        console.log(data);
+      },
+      complete: () => {
+        console.log('Done updating the user');
+      },
+    });
+  }
+
+  ondeleteUser() {
+    this.userService.deleteUser(5).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (data) => {
+        console.log(data);
+      },
+      complete: () => {
+        console.log('Done deleting the user');
+      },
     });
   }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -10,11 +10,28 @@ import { User } from '../interface/user';
 export class UserService {  
   constructor(private http: HttpClient) {}
 
+  // To fetch all Users
   getUsers(): Observable<User[]>{
     return this.http.get<User[]>(`${environment.apiUrl}/users`);
   }
 
+  // To fetch single User
   getUser(): Observable<User>{
     return this.http.get<User>(`${environment.apiUrl}/users/1`);
+  }
+
+  // To create User
+  createUser(user: User): Observable<User>{
+    return this.http.post<User>(`${environment.apiUrl}/users`,user);
+  }
+
+  //To update User
+  updateUser(user: User): Observable<User>{
+    return this.http.put<User>(`${environment.apiUrl}/users/${user.id}`,user);
+  }
+
+  //To delete User
+  deleteUser(id: number): Observable<void>{
+    return this.http.delete<void>(`${environment.apiUrl}/users/${id}`);
   }
 }
