ea0606f290004db47c95a68694e5ff158acfd621
ImVikasJi <vbhadoriya3@gmail.com>
2022-08-24 00:39:36 +0530

    Learnt about HTTP Params

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -7,37 +7,44 @@ import { User } from '../interface/user';
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {  
+export class UserService {
   constructor(private http: HttpClient) {}
 
   // To fetch all Users
-  getUsers(): Observable<User[]>{
-    let myHeaders:any = new HttpHeaders({
-      'myHeader': ['This is Vikas Header','another header value']
-    })
+  getUsers(): Observable<User[]> {
+    // HTTP Headers
+    let myHeaders: any = new HttpHeaders({
+      myHeader: ['This is Vikas Header', 'another header value'],
+    });
     myHeaders = myHeaders.set('id', '2211543');
     myHeaders = myHeaders.append('id', '102141');
-    // myHeaders = myHeaders.has('id');
-    return this.http.get<User[]>(`${environment.apiUrl}/users`,{headers: myHeaders});
+
+    // HTTP Params
+    let myParams: any = new HttpParams().set('page', '5').set('sort', 'true');
+    myParams = myParams.append('web-connect', 'true');
+    return this.http.get<User[]>(`${environment.apiUrl}/users`, {
+      headers: myHeaders,
+      params: myParams,
+    });
   }
 
   // To fetch single User
-  getUser(): Observable<User>{
+  getUser(): Observable<User> {
     return this.http.get<User>(`${environment.apiUrl}/users/1`);
   }
 
   // To create User
-  createUser(user: User): Observable<User>{
-    return this.http.post<User>(`${environment.apiUrl}/users`,user);
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${environment.apiUrl}/users`, user);
   }
 
   //To update User
-  updateUser(user: User): Observable<User>{
-    return this.http.put<User>(`${environment.apiUrl}/users/${user.id}`,user);
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${environment.apiUrl}/users/${user.id}`, user);
   }
 
   //To delete User
-  deleteUser(id: number): Observable<void>{
+  deleteUser(id: number): Observable<void> {
     return this.http.delete<void>(`${environment.apiUrl}/users/${id}`);
   }
 }
